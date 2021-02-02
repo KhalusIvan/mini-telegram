99e3f0c07e2bd1d02671923b711fa3121b649956
Khalus Ivan <vakhalus@gmail.com>
2021-02-02 22:33:24 +0200

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const server = require('http').createServer(app);
-const options = { origins:"https://mini-telegram.herokuapp.com", credentials:true, pingTimeout: 5000, pingInterval: 10000 };
+const options = { origins:"https://mini-telegram.herokuapp.com/", credentials:true, pingTimeout: 5000, pingInterval: 10000 };
 const io = require('socket.io')(server, options);
 const multer = require('multer')
 const bodyParser = require("body-parser");
